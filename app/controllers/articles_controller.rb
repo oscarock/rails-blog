1d12246c6e43ab13577a05789fbec1aae6bdacbf
Oscar Diaz <o-pipe@hotmail.com>
2017-07-23 16:21:20 -0500

    editar y colocar parciales del formulario

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     # render plain: params[:article].inspect
     # @article = Article.new(params[:article]) no se usa por seguridad
@@ -22,6 +26,16 @@ class ArticlesController < ApplicationController
    else
      render 'new'
    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
